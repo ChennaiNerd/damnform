9ecf4a95ed4c7ea7d22df2ee0170f9361f53b9aa
Fizer Khan <fizerkhan@gmail.com>
2014-04-27 01:29:05 +0530

    Updated new forms.

--- client/scripts/controllers/forms/addcontroller.js
+++ client/scripts/controllers/forms/addcontroller.js
@@ -1,24 +1,37 @@
 angular.module('myApp').controller('AddFormController',
-        function($scope, $location, Forms) {
+        function($scope, $location, $routeParams, Forms) {
 
-    var damnForm = new Forms({ name: 'Sample', schema: [] });
-    $scope.damnForm = damnForm;
+    var id = $routeParams.id;
+    if (id) {
+      $scope.damnForm = Forms.get({ id : id });
+    } else {
+      $scope.damnForm = new Forms({ name: 'Sample', schema: [] });
+    }
 
     $scope.save = function() {
-        $scope.saving = true;
-        damnForm.$save().then(function () {
+      $scope.saving = true;
+      if (id) {
+        $scope.damnForm.$update().then(function () {
             $location.path('/');
         }).finally(function() {
             $scope.saving = false;
         });
+      } else {
+        $scope.damnForm.$save().then(function () {
+            $location.path('/');
+        }).finally(function() {
+            $scope.saving = false;
+        });
+      }
     };
 
     $scope.addField = function () {
-        $scope.damnForm.schema.push({name: 'name', type:"string", mandatory: false});
+      $scope.damnForm.schema = $scope.damnForm.schema || [];
+      $scope.damnForm.schema.push({name: 'name', type:"string", mandatory: false});
     };
 
     $scope.removeField = function (index) {
-        $scope.damnForm.schema.splice(index, 1);
+      $scope.damnForm.schema.splice(index, 1);
     };
 
     $scope.fieldTypes = ['string', 'number', 'email', 'url', 'twitter', 'mobile'];
