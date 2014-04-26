24c1e7b2059ae356f3b5db5983b625d1c20ef5eb
Fizer Khan <fizerkhan@gmail.com>
2014-04-27 02:18:49 +0530

    Updated list controller and form

--- client/scripts/controllers/entries/listcontroller.js
+++ client/scripts/controllers/entries/listcontroller.js
@@ -1,6 +1,16 @@
 angular.module('myApp').controller('ListEntriesController',
-        function($scope, $rootScope, $routeParams) {
+        function($scope, $rootScope, $routeParams, Forms, Entries) {
 
-    // Write your controller stuff here
+    var id = $routeParams.id;
+    var mandatories = $scope.mandatories = [];
+    $scope.damnForm = Forms.get({ id : id });
+    $scope.damnFormEntries = Entries.query({ formId : id }, function(entries) {
+      for (var i = 0; i < entries.schema.length; i++ ) {
+        if (entries.schema[i].mandatory) {
+          mandatories.push(entries.schema[i].name);
+        }
+        console.log(mandatories);
+      }
+    });
 
 });
