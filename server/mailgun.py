c333d6c245a29039ae0689cd326fd1ea51f77c26
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2014-04-26 21:53:14 +0530

    MailGun API Configured

--- server/mailgun.py
+++ server/mailgun.py
@@ -1,12 +1,27 @@
 import requests
-def send_simple_message():
+def send_simple_message(mydata):
     return requests.post(
         "https://api.mailgun.net/v2/sandbox381bc65cf9a0430cb057afb272d83c3a.mailgun.org/messages",
         auth=("api", "key-1e4mr4g9yfnhyfhps50ks2tu3xabjok1"),
-        data={"from": "Mailgun Sandbox <postmaster@sandbox381bc65cf9a0430cb057afb272d83c3a.mailgun.org>",
-              "to": "Dheeraj <dheerajjoshi1991@gmail.com>",
-              "subject": "Hello Dheeraj",
-              "text": "Congratulations Dheeraj, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."})
+        data={"from": mydata.get('from'),
+              "to": mydata.get('to'),
+              "cc": mydata.get('cc'),
+              "bcc": mydata.get('bcc'),
+              "subject": mydata.get('subject'),
+              "text": mydata.get('text', ''),
+              "html": mydata.get('html', '') })
+
+def get_data():
+	return {
+				"from": "Mailgun Sandbox <postmaster@sandbox381bc65cf9a0430cb057afb272d83c3a.mailgun.org>",
+				"to": "Dheeraj <dheerajjoshi1991@gmail.com>",
+				"cc": ["Dj <dheerajjoshi@outlook.com>","Joshi <dheerajjoshi1991@yahoo.co.in>"],
+				"bcc": ["Dj <dheerajjoshi@outlook.com>"],
+				"subject": "Hello World",
+				#"text": "Testing Text :::: 1234567890"
+				"html": "<html>Testing HTML <b>Bold</b><i>Italics</i><u>Underline</u></html>"
+			}
 
 if __name__ == "__main__":
-	send_simple_message()
+	mydata = get_data()
+	send_simple_message(mydata)
