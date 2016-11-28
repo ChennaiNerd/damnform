dbc125a1530cca865ff355a4e25240b1f408fa30
Fizer Khan (பைசர் கான்) <fizerkhan@gmail.com>
2016-11-28 15:34:21 +0530

    Updated Mailgun

--- server/mailgun.py
+++ server/mailgun.py
@@ -2,7 +2,7 @@ import requests
 def send_simple_message(mydata):
     return requests.post(
         "https://api.mailgun.net/v2/sandbox381bc65cf9a0430cb057afb272d83c3a.mailgun.org/messages",
-        auth=("api", "key-1e4mr4g9yfnhyfhps50ks2tu3xabjok1"),
+        auth=("api", "APIKEY"),
         data={"from": mydata.get('from'),
               "to": mydata.get('to'),
               "cc": mydata.get('cc'),
